496634f1401345b2494bf5db660150139bbbe017
Josias Torres <josiastorresvillegas@Josiass-MacBook-Pro.local>
2024-11-13 19:24:36 -0600

    Initial commit - Add Typescript CLI project

---
+++ src/classes/Motorbike.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// TODO: The Motorbike class should extend the Vehicle class
+class Motorbike {
+}
+// Export the Motorbike class as the default export
+exports.default = Motorbike;
